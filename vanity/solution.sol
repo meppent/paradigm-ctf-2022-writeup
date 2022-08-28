1afb958350f7d1d893e0782536f2f5328b7a2ac5
makcandrov <makcandrov@proton.me>
2022-08-28 03:33:58 -0700

    License and formatting

--- vanity/solution.sol
+++ vanity/solution.sol
@@ -1,21 +1,25 @@
-pragma 0.7.6;
+// SPDX-License-Identifier: GNU AGPLv3
+
+pragma solidity 0.7.6;
 import "./Challenge.sol";
 
-interface ISetup{
-    function challenge() returns(address);
+interface ISetup {
+    function challenge() returns (address);
 }
 
-contract Solution{
-    constructor(address _setup) public{
-        Challenge challenge = ISetup( _setup).challenge();
+contract Solution {
+    constructor(address _setup) public {
+        Challenge challenge = ISetup(_setup).challenge();
         signature = "";
 
-        assembly{
+        assembly {
             mstore(signature, 0x00)
-            mstore(add(signature, 0x20), 0x3f7f000000000000000000000000000000000000000000000000000051f1441a)
+            mstore(
+                add(signature, 0x20),
+                0x3f7f000000000000000000000000000000000000000000000000000051f1441a
+            )
         }
 
         challenge.solve(address(2), signature);
     }
-
 }
